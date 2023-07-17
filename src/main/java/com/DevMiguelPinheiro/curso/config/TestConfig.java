ecd81f801e52b036ab6592f7fabcadb2d9acd669
Miguel Pinheiro Leite <leitemiguelpinheiro@gmail.com>
2023-07-17 10:17:01 -0300

    create class payment

--- src/main/java/com/DevMiguelPinheiro/curso/config/TestConfig.java
+++ src/main/java/com/DevMiguelPinheiro/curso/config/TestConfig.java
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.Profile;
 import com.DevMiguelPinheiro.curso.entities.Category;
 import com.DevMiguelPinheiro.curso.entities.Order;
 import com.DevMiguelPinheiro.curso.entities.OrderItem;
+import com.DevMiguelPinheiro.curso.entities.Payment;
 import com.DevMiguelPinheiro.curso.entities.Product;
 import com.DevMiguelPinheiro.curso.entities.User;
 import com.DevMiguelPinheiro.curso.entities.enums.OrderStatus;
@@ -87,6 +88,11 @@ public class TestConfig implements CommandLineRunner {
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
 		
+
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+
+		orderRepository.save(o1);
 	}
 
 }
